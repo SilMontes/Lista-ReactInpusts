975cc5fd0a6879ec30c2c3269cb744a51772d796
SilMontes <Silmontes2001@gmail.com>
2021-03-09 06:09:04 +0000

    lista filter y concat

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,56 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [lista, setLista] = useState([]);
+	const [contenido, setContenido] = useState();
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-3">
+			<h1>todos</h1>
+			<div className="pendientes">
+				<input
+					key={"i"}
+					value={contenido}
+					onChange={e => {
+						setContenido(e.target.value);
+					}}
+					onKeyPress={event => {
+						if (event.key === "Enter") {
+							if (contenido !== "") {
+								setLista(lista.concat(contenido));
+								setContenido("");
+							} else {
+								alert("Add a task");
+							}
+						}
+					}}
+				/>
+				{/* <button onClick={()=>{
+                if(contenido != ""){
+                    setLista(lista.concat(contenido));
+                    setContenido("");
+                }else
+            }}></button> */}
+				{lista.map((item, index) => {
+					return (
+						<div
+							key={index}
+							onClick={() => {
+								setLista(
+									lista.filter(
+										(item1, index1) => index1 != index
+									)
+								);
+							}}>
+							{item}
+						</div>
+					);
+				})}
+				<br />
+			</div>
+			<div className="countingtasks">
+				<p>{"Total: " + lista.length}</p>
+			</div>
 		</div>
 	);
 }
